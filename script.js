339dbcdfb79341e8377c260cc7792de4c5846acb
johntvale <johntvale@gmail.com>
2021-06-07 19:20:45 -0400

    requisitos 7 e 8 resolvidos.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const cButton = document.querySelector('#criar-tarefa');
+const criarTarefa = document.querySelector('#criar-tarefa');
 
-function criarElementoLista() {
+function mudarCorFundo(adicionaRecebimentoDeClick) { // evento aciona apenas quando for CLICADO
+  const clicado = adicionaRecebimentoDeClick.target;
+  const arrayLista = document.querySelectorAll('li');
+
+  for (let index = 0; index < arrayLista.length; index += 1) { // confere se alguém já foi clicado (se já tem fundo cinza)
+    if (arrayLista[index].className === 'corClicado') {
+      arrayLista[index].className = '';
+      // console.log('perdeu a classe cinza ' + arrayLista[length]);
+    }
+  }
+  clicado.className = 'corClicado';
+}
+
+// criar link em cada adição na lista
+function adicionaRecebimentoDeClick() {
+  const ultimaPosicao = document.querySelectorAll('li').length - 1;
+  const ultimoItemDaLista = document.querySelectorAll('li')[ultimaPosicao];
+  console.log(ultimoItemDaLista);
+  ultimoItemDaLista.addEventListener('click', mudarCorFundo);
+}
+
+function criarElementoLista() { // CRIAR LIST ITEM
   // Recupera o texto digitado
   const caixaInput = document.getElementById('texto-tarefa');
   const textoDigitado = document.getElementById('texto-tarefa').value;
-  console.log(textoDigitado);
 
   // Cria o item da lista, e atribui nele o texto digitado
   const listatarefas = document.getElementById('lista-tarefas');
@@ -14,6 +34,8 @@ function criarElementoLista() {
 
   // limpa caixa de texto
   caixaInput.value = '';
+
+  adicionaRecebimentoDeClick(); // ADICIONA RECEBIMENTO DE CLICK AO ULTIMO ITEM DA LISTA
 }
 
-cButton.addEventListener('click', criarElementoLista);
+criarTarefa.addEventListener('click', criarElementoLista); // botão TAREFA >> CRIAR List Item
