75e2c017606197c99916448be61cdc8d97769e2c
johntvale <johntvale@gmail.com>
2022-07-27 20:10:05 -0400

    Correções de lint e remoção de arquivos de teste da trybe

--- script.js
+++ script.js
@@ -2,11 +2,40 @@ const criarTarefa = document.querySelector('#criar-tarefa');
 const apagarLista = document.querySelector('#apaga-tudo');
 const limparCompletos = document.querySelector('#remover-finalizados');
 
-criarTarefa.addEventListener('click', criarElementoLista); // botão ADICIONAR cria LI
+function mudarCorFundo(handleOneClick) { // evento aciona apenas quando for CLICADO
+  const clicado = handleOneClick.target;
+  const arrayLista = document.querySelectorAll('li');
 
-apagarLista.addEventListener('click', apagarTodosItensLista); // botão LIMPAR LISTA remove todas as LI
+  for (let index = 0; index < arrayLista.length; index += 1) { // confere se alguém já foi clicado (se já tem fundo cinza)
+    if (arrayLista[index].className === 'corClicado') {
+      arrayLista[index].classList.remove('corClicado');
+    }
+  }
+  clicado.classList.add('corClicado');
+}
 
-limparCompletos.addEventListener('click', apagarItensFinalizados); // botão LIMPAR LISTA remove todas as LI
+// criar link em cada adição na lista
+function adicionarRecebimentoDeClick() {
+  const ultimaPosicao = document.querySelectorAll('li').length - 1;
+  const ultimoItemDaLista = document.querySelectorAll('li')[ultimaPosicao];
+  ultimoItemDaLista.addEventListener('click', mudarCorFundo);
+}
+
+// adiciona "corte no texto" caso receba 2 clicks, retirar caso receba 2 clicks novamente
+function adicionarConcluidoDoubleClick(handleDoubleClick) {
+  const clicado2x = handleDoubleClick.target;
+  if (clicado2x.className === 'corClicado') {
+    clicado2x.classList.add('completed');
+  } else if (clicado2x.className === 'corClicado completed') {
+    clicado2x.classList.remove('completed');
+  }
+}
+
+function adicionarRecebimentoDeDoubleClick() { // adiciona função de double click em todos os novos itens de lista
+  const ultimaPosicao2 = document.querySelectorAll('li').length - 1;
+  const ultimoItemDaLista2 = document.querySelectorAll('li')[ultimaPosicao2];
+  ultimoItemDaLista2.addEventListener('dblclick', adicionarConcluidoDoubleClick);
+}
 
 function criarElementoLista() { // CRIAR LIST ITEM
   const caixaInput = document.getElementById('texto-tarefa'); // caixa input
@@ -25,41 +54,6 @@ function criarElementoLista() { // CRIAR LIST ITEM
   adicionarRecebimentoDeDoubleClick(); // ADICIONA class COMPLETE, com 2 click
 }
 
-// criar link em cada adição na lista
-function adicionarRecebimentoDeClick() {
-  const ultimaPosicao = document.querySelectorAll('li').length - 1;
-  const ultimoItemDaLista = document.querySelectorAll('li')[ultimaPosicao];
-  ultimoItemDaLista.addEventListener('click', mudarCorFundo);
-}
-
-function mudarCorFundo(adicionarRecebimentoDeClick) { // evento aciona apenas quando for CLICADO
-  const clicado = adicionarRecebimentoDeClick.target;
-  const arrayLista = document.querySelectorAll('li');
-
-  for (let index = 0; index < arrayLista.length; index += 1) { // confere se alguém já foi clicado (se já tem fundo cinza)
-    if (arrayLista[index].className === 'corClicado') {
-      arrayLista[index].classList.remove('corClicado');
-    }
-  }
-  clicado.classList.add('corClicado');
-}
-
-function adicionarRecebimentoDeDoubleClick() { // adiciona função de double click em todos os novos itens de lista
-  const ultimaPosicao2 = document.querySelectorAll('li').length - 1;
-  const ultimoItemDaLista2 = document.querySelectorAll('li')[ultimaPosicao2];
-  ultimoItemDaLista2.addEventListener('dblclick', adicionarConcluidoDoubleClick);
-}
-
-// adiciona "corte no texto" caso receba 2 clicks, retirar caso receba 2 clicks novamente
-function adicionarConcluidoDoubleClick(adicionarRecebimentoDeDoubleClick) {
-  const clicado2x = adicionarRecebimentoDeDoubleClick.target;
-  if (clicado2x.className === 'corClicado') {
-    clicado2x.classList.add('completed');
-  } else if (clicado2x.className === 'corClicado completed') {
-    clicado2x.classList.remove('completed');
-  }
-}
-
 // remove todos as LI da lista OL
 function apagarTodosItensLista() {
   const itensLista = document.querySelectorAll('li');
@@ -71,10 +65,16 @@ function apagarTodosItensLista() {
 function apagarItensFinalizados() {
   const itensLista = document.querySelectorAll('li');
   for (let index3 = 0; index3 < itensLista.length; index3 += 1) {
-    if (itensLista[index3].className === 'completed') {
-      itensLista[index3].remove();
-    } else if (itensLista[index3].className === 'corClicado completed') {
+    if (
+      (itensLista[index3].className === 'completed')
+      || (itensLista[index3].className === 'corClicado completed')) {
       itensLista[index3].remove();
     }
   }
 }
+
+criarTarefa.addEventListener('click', criarElementoLista); // botão ADICIONAR cria LI
+
+apagarLista.addEventListener('click', apagarTodosItensLista); // botão LIMPAR LISTA remove todas as LI
+
+limparCompletos.addEventListener('click', apagarItensFinalizados); // botão LIMPAR LISTA remove todas as LI
